b824495625500041d82f1803f84b055398f0d505
kdkcarisma <sht1014@naver.com>
2015-11-06 15:01:05 +0900

    first commit

---
+++ src/main/java/com/hybrid/CommonsLogTest.java
@@ -0,0 +1,21 @@
+package com.hybrid;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+public class CommonsLogTest {
+
+	static Log log = LogFactory.getLog(CommonsLogTest.class);
+	
+	public static void main(String[] args) {
+		
+	}
+	
+	public static void test1() {
+		log.info("Hello Commons Logging");
+		
+		for (int i=0; i<10; i++) {
+			log.info("i = "+ i);
+	 }
+   }
+}
